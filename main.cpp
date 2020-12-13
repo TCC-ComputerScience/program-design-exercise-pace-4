9a56834e67fefa286c66562806804f137141e531
zachnahorney <75685010+zachnahorney@users.noreply.github.com>
2020-12-12 22:53:14 -0500

    Update main.cpp
    just comments

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
-//Jason Brewer
-//Zach Nahorney
-//Bryan Aguinaldo
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+#include <cstring>
 #include <string>
 #include <iomanip>
 #include <unistd.h>
 
 using namespace std;
 
-//Constant integer to hold the size of the board
-const int boardSize = 8;
+@@ -13,11 +15,11 @@ const int boardSize = 8;
 
 struct Ship
 {
+    char[2] patrolBoat;
+    char[4] battleship;
+    char[3] submarine;
+    char[3] cruiser;
+    char[5] carrier;
     char patrolBoat[2];
     char battleship[4];
     char submarine[3];
@@ -23,41 +25,26 @@ struct Ship
 };
 
 //This prints the grid to screen
-//Pre: Board with hit/miss info already initialized
-//Post: prints this board to screen
-void printGrid(char hitMissBoard[boardSize][boardSize]);
-
-//This resets the board and generates the ships using srand and rand time
-//Pre: Gather data to generate an 8x8 grid
-//Post: Grid with ships generated randomly
-void generateGrid(char shipsBoard[boardSize][boardSize], char hitMissBoard[boardSize][boardSize]);
-
+@@ -33,12 +35,12 @@ void generateGrid(char shipsBoard[boardSize][boardSize], char hitMissBoard[board
 //This function asks the user to input coordinates, then converts the user input into coordinates
 //Pre: Input from user in the format "A2"
 //Post: Returns "A2" in the format "1" "2"
+void convertInput(int& X, int& Y);
 void convertInput(int& X, int& Y, string input);
 
 //This function prints the rules to the game and how the user will play it
 //Pre:
 //Post: menu printed to screen
+void Menu();
 char Menu(char yesNo);
 
 //This searches the grid to check if it's hit or miss
 //Pre: Board with ships generated and user input already converted
-//Post: returns whether it was a hit or a miss
-bool hitMissCheck(char shipsBoard[boardSize][boardSize], int X, int Y);
-
-//This function updates the board with information on whether it was a hit or a miss
-//Pre: Board with hit/misses from user input already, and user input already converted
-//Post:
-void updateBoard(char hitMissBoard[boardSize][boardSize], int X, int Y);
-
-int main()
-{
-    //2d array of size boardSize with hit/miss data
+@@ -56,14 +58,28 @@ int main()
     char hitMissBoard [boardSize][boardSize];
     //2d array of size boardSize with ship data
     char shipsBoard [boardSize][boardSize];
+    //X and Y integers to hold the user inputted coordinates
     //string input for user input and X and Y integers to hold the user inputted coordinates once converted
     string input;
     int X;
@@ -67,10 +54,12 @@ int main()
     //Bool to hold whether it was a hit or miss
     bool hitMiss;
 
+    Menu();
+    convertInput(X, Y);
     yesNo = Menu(yesNo);
     if(yesNo =='Y')
     {
-        cout << "Enter the spot you would like to shoot (format: A4 or C7): ";
+        cout << "Enter the spot you would like to shoot (format: A4 or C7): ";//enter format
         do
         {
             cin >> input;
@@ -83,15 +72,14 @@ int main()
     generateGrid(shipsBoard, hitMissBoard);
     printGrid(hitMissBoard);
     hitMissCheck(shipsBoard, X, Y);
-    updateBoard(hitMissBoard, X, Y);
-
-    return 0;
-
+@@ -74,14 +90,77 @@ int main()
 
 }
 
+void Menu()
 char Menu(char yesNo)
 {
+    cout << "This prints the menu" << endl;
     cout << "              _           _   _   _           _     _       " << endl;
     sleep(1);
     cout << "             | |         | | | | | |         | |   (_)      " << endl;
@@ -106,7 +94,7 @@ char Menu(char yesNo)
     sleep(1);
     cout << "                                                     | |    " << endl;
     sleep(1);
-    cout << "                                                     |_|    " << endl << endl;
+    cout << "                                                     |_|    " << endl << endl;//prints out battleship
     sleep(1);
     cout << "                                     |__" << endl;
     cout << "                                     |\\/" << endl;
@@ -120,37 +108,39 @@ char Menu(char yesNo)
     cout << "                ____`-' ||___-{]_| _[}-  |     |_[___\\==--            \\/   _" << endl;
     cout << " __..._____--==/___]_|__|_____________________________[___\\==--____,------' .7" << endl;
     cout << "|                                                                     BB-61/" << endl;
-    cout << " \\_________________________________________________________________________|" << endl;
+    cout << " \\_________________________________________________________________________|" << endl;//creates cool image of boaty
     cout << "Art By: Matthew Bace" << endl << endl << endl;
     //sleep(3);
     cout << "The object of Battleship is to try and sink all enemy ships."<< endl;
     cout << "All ships are hidden to you and you must use grid coordinates"<< endl;
-    cout << "to fire missiles to discover ships locations. If you hit a"<< endl;
+    cout << "to fire missiles to discover ships locations. If you hit a"<< endl;//Menu EXPLANATION
     cout << "ship, it is represented with a * and if you miss, it is rep-"<< endl;
     cout << "resented with a H." << endl << endl;
     //sleep(5);
-    cout << "Would you like to play? (Y or N)";
+    cout << "Would you like to play? (Y or N)";//promts the user to say if they wanna play
     do
     {
         cin >> yesNo;
         if(yesNo == 'Y')
-            cout << "Let's get started!" << endl;
+            cout << "Let's get started!" << endl;//if YES
         else if(yesNo == 'N')
-            cout << "why did you even launch the program then...?" << endl;
+            cout << "why did you even launch the program then...?" << endl;//IF NO
         else
-            cout << "Please enter either Y or N" << endl;
+            cout << "Please enter either Y or N" << endl;//did not input a valid datatype
     }while(yesNo != 'Y' && yesNo != 'N');
     return yesNo;
 }
 
+void convertInput(int& X, int& Y)
 void convertInput(int& X, int& Y, string input)
 {
+    cout << "This converts the user's input" << endl;//Converts the users value for board identifiers ex: A1
     switch(input[0])
     {
-        case 'A' : Y = 0;
-        case 'B' : Y = 1;
-        case 'C' : Y = 2;
-        case 'D' : Y = 3;
+        case 'A' : Y = 0;//for the first half of the identifier
+        case 'B' : Y = 1;//it is assigned
+        case 'C' : Y = 2;//to a char
+        case 'D' : Y = 3;//and then it is assigned to an int
         case 'E' : Y = 4;
         case 'F' : Y = 5;
         case 'G' : Y = 6;
@@ -160,26 +150,7 @@ void convertInput(int& X, int& Y, string input)
     X = input[1];
     cout << endl << "X: " << X;
     cout << endl << "Y: " << Y;
-
+    cout << endl;
 }
 
 void generateGrid(char shipsBoard[boardSize][boardSize], char hitMissBoard[boardSize][boardSize])
-{
-    cout << "this generates a new/clean board and places ships on it." << endl;
-}
-
-void printGrid(char hitMissBoard[boardSize][boardSize])
-{
-    cout << "This prints the grid with ships hidden" << endl;
-}
-
-bool hitMissCheck(char shipsBoard[boardSize][boardSize], int X, int Y)
-{
-    cout << "This searches the grid to check if it's hit or miss" << endl;
-    return true;
-}
-
-void updateBoard(char hitMissBoard[boardSize][boardSize], int X, int Y)
-{
-    cout << "This function updates the board with information on whether it was a hit or a miss" << endl;
-}
